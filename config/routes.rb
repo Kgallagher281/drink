8eb800f5c23d8c5b7ec8a6fbcccfab15a9cba768
kgallagher <kevin.gallagher23@gmail.com>
2017-11-19 13:00:14 -0800

    home controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   devise_for :users
+  root 'home#index'
 end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-
-root  :to 'home#index'
